c746ebb599dbf45d4e9464bfc88e8a2b9ec5e0fb
Subin Saji <subinssaji@gmail.com>
2024-02-16 15:19:21 +0000

    support for cryocooler streamDevice

---
+++ oxcs700/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+# ARGUMENTS:
+#  $1 VERSION to install (must match repo tag)
+VERSION=${1}
+NAME=oxcs700
+FOLDER=$(dirname $(readlink -f $0))
+
+# log output and abort on failure
+set -xe
+
+# doxygen is used in documentation build for the developer stage
+ibek support apt-install --only=dev doxygen
+
+# get the source and fix up the configure/RELEASE files
+ibek support git-clone ${NAME} ${VERSION} --org https://github.com/epics-containers/
+
+ibek support register ${NAME}
+
+# declare the libs and DBDs that are required in ioc/iocApp/src/Makefile
+# None required for a stream device ------------------------------------
+#ibek support add-libs
+#ibek support add-dbds
+
+# compile the support module
+ibek support compile ${NAME}
+# prepare *.bob, *.pvi, *.ibek.support.yaml for access outside the container.
+ibek support generate-links ${FOLDER}
